9d4d72594f4702e028a9117d005e9e9182dc0caf
llmmcc <97648020+llmmcc@users.noreply.github.com>
2025-12-09 19:07:38 +0800

    Update mod.rs

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -3,3 +3,11 @@ mod types;
 
 pub use auth::*;
 pub use types::*;
+pub mod offline;
+
+use offline::OfflineAuthProvider;
+
+pub enum AuthProvider {
+    Http(HttpAuthProvider),
+    Offline(OfflineAuthProvider), // 添加离线认证
+}
